6afc46e9a695ef798bde585154ec4ddaeda5c3c9
JesusRiestra <jriestraricote@gmail.com>
2022-12-07 11:08:50 +0100

    Haciendo más robusto el metodo getNombreUsuarioGithub 1.0.0

--- Alumno.java
+++ Alumno.java
@@ -1,5 +1,5 @@
 public class Alumno {
-    
+
     // el nombre completo del alumno
     private String nombre;
     // el numero de matricula
@@ -17,7 +17,7 @@ public class Alumno {
         if (nombre.length() < 3) {
             System.out.println("El nombre del alumno es demasiado corto!");
         }
-        
+
         if (numeroMatricula.length() < 4) {
             System.out.println("El número de matricula es demasiado corto!");
         }
@@ -29,26 +29,39 @@ public class Alumno {
     public String getNombre() {
         return nombre;
     }
-    
+
     /**
      * Cambia el nombre del alumno
      */
     public void cambiarnombre(String nuevoNombre) {
         nombre = nuevoNombre;
     }
-    
+
     /**
      * Imprime por pantalla los detalles del alumno
      */
     public void imprimeDetalles() {
         System.out.println(nombre + " (" + numeroMatricula + ") - " + edad + " aÃ±os");
     }
-    
+
     /**
      * Devuelve el nombre de usuario que el alumno debe configurar
      * en su cuenta de Github en formato de 7 caracateres
      */
     public String getNombreUsuarioGithub() {
-        return nombre.substring(0, 3) + numeroMatricula.substring(0, 4);
+        String valorADevolver = "";
+        if (nombre.length() >= 3 && numeroMatricula.length() >= 4) {
+            valorADevolver = nombre.substring(0, 3) + numeroMatricula.substring(0, 4);
+        }
+        else if (nombre.length() >= 3 && numeroMatricula.length() < 4) {
+            valorADevolver = nombre.substring(0, 3) + numeroMatricula;
+        }
+        else if (nombre.length() < 3 && numeroMatricula.length() >= 4) {    
+            valorADevolver = nombre + numeroMatricula.substring(0, 4);
+        }
+        else if (nombre.length() < 3 && numeroMatricula.length() < 4) {
+            valorADevolver = nombre + numeroMatricula;
+        }
+        return valorADevolver;
     }
 }
